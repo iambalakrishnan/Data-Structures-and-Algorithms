0b77aa8e4a31e4ed76f7f5348fb7ef49f3b386a7
iambalakrishnan <imbkrishnaa@gmail.com>
2023-03-17 15:02:01 +0530

    how_LL_works.py added

---
+++ LinkedList/how_LL_works.py
@@ -0,0 +1,21 @@
+head = {
+    "value":11,
+    "next": {
+                "value":3,
+                "next": {
+                            "value":23,
+                            "next": {
+                                        "Value":7,
+                                        "next": {
+                                                    "Value":4,
+                                                    "next":None
+                                        }
+                            }
+                }
+    }
+}
+
+print(head['next']['next']['value'])
+
+# This will only run with a linked list
+# print(my_linked_list.head.next.next.value)
