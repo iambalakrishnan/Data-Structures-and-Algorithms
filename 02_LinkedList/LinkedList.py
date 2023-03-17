e83b25f222d13510959623ab439cfce32754e9f8
iambalakrishnan <imbkrishnaa@gmail.com>
2023-03-17 18:06:42 +0530

    Linkedlist append and pop mentod implemented

--- 02_LinkedList/LinkedList.py
+++ 02_LinkedList/LinkedList.py
@@ -11,7 +11,73 @@ class LinkedList:
         self.tail = new_node
         self.length = 1
 
+    
+    def print_linkedlist(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
 
-my_linked_list = LinkedList(4) 
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
 
-print(my_linked_list.head.value)
+    def pop(self):
+    
+    #Check the node is already none
+        if self.length == 0:
+            return None
+
+        pre = self.head
+        temp = self.head
+        while(temp.next):
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+
+        # After deleting node with one element
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        
+
+        return [temp, temp.value]
+    
+
+
+
+# #creating node and append operation 
+# my_linked_list = LinkedList(1)
+# my_linked_list.append(2)
+# my_linked_list.append(3)
+# my_linked_list.print_linkedlist()
+
+
+# Pop operation for multiple element
+my_linked_list = LinkedList(1)
+my_linked_list.append(2)
+# (2) Items - Return 2 Node
+print(my_linked_list.pop())
+# (1) Items - Return 1 Node
+print(my_linked_list.pop())
+# (0) Items - Return 0 Node
+print(my_linked_list.pop())
+my_linked_list.print_linkedlist()
+
+# # Pop operation for single element
+# my_linked_list = LinkedList(1)
+# my_linked_list.print_linkedlist()
+# # (0) Items - Return 0 Node
+# print(my_linked_list.pop())
+
+
+
+
