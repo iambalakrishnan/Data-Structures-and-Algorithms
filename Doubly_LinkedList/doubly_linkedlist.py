9fc1f16c5c2576742e6d8054f22a65f43f89d62a
iambalakrishnan <imbkrishnaa@gmail.com>
2023-03-19 20:33:22 +0530

    doubly_linkedlist - pop first, get set insert remove reverse method implemented

--- Doubly_LinkedList/doubly_linkedlist.py
+++ Doubly_LinkedList/doubly_linkedlist.py
@@ -62,26 +62,137 @@ class DoublyLinkedList:
 
 
     def pop_first(self):
-        pass
+        
+        if self.length == 0:
+            return None
+        temp = self.head
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.head = self.head.next
+            self.head.prev = None
+            temp.next = None
+        self.length -= 1
+        #return [temp, temp.value]
+        return temp
 
-    def insert(self):
-        pass
+    def get(self, index):
+        if index < 0 or index > self.length:
+            return None
+        temp = self.head
+        if index < self.length/2:
+            for _ in range(index):
+                temp = temp.next
+        else:
+            temp = self.tail
+            for _ in range(self.length - 1, index, -1):
+                temp = temp.prev
+        # return [temp, temp.value]
+        return temp
+    
+    def set_value(self, index, value):
+        temp = self.get(index)
+        if temp:
+            temp.value = value
+            return True
+        return False
+        
+    def insert(self, index, value):
+        if index < 0 or index > self.length:
+            return False
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+        
+        new_node = Node(value)
+        before = self.get(index - 1)
+        after = before.next
 
-    def remove(self):
-        pass
+        new_node.prev = before
+        new_node.next = after
+        before.next = new_node
+        after.prev = new_node
+        
+        self.length += 1 
+        return True
+
+    def remove(self, index):
+        if index < 0 or index > self.length:
+            return None # here we are returning a node
+        if index == 0:
+            print(self.length)
+            return self.pop_first()
+        if index == self.length - 1:
+            print(f"length is {self.length}")
+            return self.pop()
+        temp = self.get(index)
+        
+        temp.next.prev =  temp.prev
+        temp.prev.next = temp.next
+        temp.prev = None
+        temp.next = None
+        
+        self.length -= 1
+        # return [temp, temp.value]
+        return temp
+        
 
     def reverse(self):
-        pass
+        first = None
+        second = self.head
+        
+        while second is not None:
+            first = second.prev
+            second.prev = second.next
+            second.next = first
+            second = second.prev
 
-# Create and append first doubly linkedlist element
-my_doubly_linkedlist = DoublyLinkedList(2)
-my_doubly_linkedlist.append(3)
-my_doubly_linkedlist.print_linkedlist()
-my_doubly_linkedlist.prepend(1)
-my_doubly_linkedlist.print_linkedlist()
+        if first is not None:
+            self.head = first.prev  
+
+# # Create and append first doubly linkedlist element
+# my_doubly_linkedlist = DoublyLinkedList(0)
+# my_doubly_linkedlist.append(1)
+# my_doubly_linkedlist.append(2)
+# my_doubly_linkedlist.print_linkedlist()
+# #prepend 
+# my_doubly_linkedlist.prepend(1)
 # # (2) Items - Return 2nd Node
 # print(my_doubly_linkedlist.pop())
 # # (1) Items - Return 1st Node
 # print(my_doubly_linkedlist.pop())
 # # (0) Items - Return None
 # print(my_doubly_linkedlist.pop())
+
+# # pop first
+# print(my_doubly_linkedlist.pop_first())
+# print(my_doubly_linkedlist.pop_first())
+# print(my_doubly_linkedlist.pop_first())
+
+# # get()
+# print(my_doubly_linkedlist.get(0))
+# print(my_doubly_linkedlist.get(2))
+
+# # set_value()
+# print(my_doubly_linkedlist.set_value(0,10))
+# my_doubly_linkedlist.print_linkedlist()
+# print(my_doubly_linkedlist.set_value(2,20))
+# my_doubly_linkedlist.print_linkedlist()
+
+# # insert()
+# my_doubly_linkedlist.insert(1,10)
+# my_doubly_linkedlist.print_linkedlist()
+
+# # remove()
+# print(my_doubly_linkedlist.remove(1), "\n")
+# my_doubly_linkedlist.print_linkedlist()
+
+# Reverse a linkedlist
+my_doubly_linkedlist = DoublyLinkedList(0)
+my_doubly_linkedlist.append(1)
+my_doubly_linkedlist.append(2)
+my_doubly_linkedlist.print_linkedlist()
+my_doubly_linkedlist.reverse()
+my_doubly_linkedlist.print_linkedlist()
